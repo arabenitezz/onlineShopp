0a20292d29ddb3864e6ea35865b756ae02f0dd86
arabenitezz <abenitezr18@gmail.com>
2024-12-10 00:55:42 -0300

    Server config made

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
